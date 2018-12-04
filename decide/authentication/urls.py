3a8967d98fdc5403168a7a7283b20d544b78e9d7
Pablo Pino <pabpinjim@alum.us.es>
2018-12-04 19:58:34 +0100

    Añadidas urls de autenticación mediante email
    Con la url /authentication/request-auth-email-code/ el usuario accede a
    la vista en la que puede pedir su código para la autenticación mediante
    email.
    
    Con la url /authentication/login-auth-email/ el usuario accede a la vista
    en la que puede usar el código que ha recibido mediante email para
    autenticarse.

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView, RegisterUserView
+from .views import GetUserView, LogoutView, RegisterUserView, RequestAuthEmailCodeView, LoginEmailCodeView
 
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterUserView.as_view()),
+    path('request-auth-email-code/', RequestAuthEmailCodeView.as_view()),
+    path('login-auth-email/', LoginEmailCodeView.as_view()),
 ]
