46eb451216b816fc67bcfc09f9b936ba844c1b23
Javier Castillo Delgado <44399661+javcasdel@users.noreply.github.com>
2018-12-07 00:00:04 +0100

    Feature: añadida estructura básica de ley d'Hondt; #1
    Se ha añadido la multiplexión básica de diferentes tipos de recuento en las vistas de postprocesado.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -41,9 +41,49 @@ class PostProcView(APIView):
         options.sort(key=lambda x: -x['seats'])
         return Response(options)
 
+
+    def multiQuestions(self, questions):
+
+        for question in questions:
+            for option in question:
+                option['postproc'] = option['votes'];
+
+            question.sort(key=lambda x: -x['postproc'])
+
+        
+        return Response(questions)
+        
+    def equalityVoting(self, options, seats, method):
+
+        groups = []
+        divOptions = {}
+        res = []
+        #Obtener grupos
+        for opt in options:
+            g = opt.get("group", "no_group")
+            
+            if not g in groups: 
+                groups.append(g)
+
+        #Inicializar listas de opciones
+        for group in groups:
+            divOptions[group] = []      
+
+        #Categorizar opciones por grupo
+        for opt in options:
+            divOptions[opt.get("group", "no_group")].append(opt)
+
+        #Unificar votaciones
+        for group in dict(divOptions):
+            res = [*res, *method(divOptions[group], seats//len(groups)).data]
+
+        res.sort(key=lambda x: -x['seats'])
+        return Response(res)
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
+         * groups: true
          * options: [
             {
              option: str,
@@ -55,10 +95,23 @@ class PostProcView(APIView):
         """
 
         t = request.data.get('type', 'IDENTITY')
+        groups = request.data.get('groups', False)
         opts = request.data.get('options', [])
+        if groups:
+            if t == 'DHONDT':
+                seats = int(float(request.data.get('seats', '8')))
+                return self.equalityVoting(opts, seats, lambda x, y : self.dhondt(x, y))
 
-        if t == 'IDENTITY':
+            elif t == 'SAINTELAGUE':
+                seats = int(float(request.data.get('seats', '8')))
+                return self.equalityVoting(opts, seats, lambda x, y : self.sainteLague(x, y))
+
+        elif t == 'IDENTITY':
             return self.identity(opts)
+
+        elif t == 'MULTI':
+            questions = request.data.get('questions', [])
+            return self.multiQuestions(questions)
 
         elif t == 'DHONDT':
             seats = int(float(request.data.get('seats', '8')))
