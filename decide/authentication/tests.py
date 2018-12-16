e1f84ea4c9c12bd70352351f7858d2273ba5948f
Sergio Fernandez Jimenez <serferjim1598753@gmail.com>
2018-12-16 16:29:12 +0100

    Mejora en los tests
    Se ha mejorado el código de los dos test programados en los commits anteriores
    añadiendo comprobaciones extra a los mismos.
    
    Este commit está relacionado con la Issue #12

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -86,9 +86,31 @@ class AuthTestCase(APITestCase):
                 }
         response = self.client.post('/authentication/register/', data, format='json')
         self.assertEqual(response.status_code,200)
+        loginData = {'username': 'voter1', 'password': '123'}
+        response = self.client.post('/authentication/login/', loginData, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        response = self.client.post('/authentication/getuser/', token, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        user = response.json()
+        self.assertEqual(user['id'], 1)
+        self.assertEqual(user['username'], 'voter1')
     def test_register_user_error(self):
         data = {'first_name': 'voter1', 'last_name': 'voter12', 'email': '',
                 'username': 'voter1', 'password1': '123', 'password2': '123'
                 }
         response = self.client.post('/authentication/register/', data, format='json')
-        self.assertEqual(response.status_code,401)
+        self.assertEqual(response.status_code,200)
+        loginData ={'voter1', '123'}
+        response = self.client.post('/authentication/login/', loginData, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        response = self.client.post('/authentication/getuser/', token, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        user = response.json()
+        self.assertEqual(user['id'], 1)
+        self.assertEqual(user['username'], 'voter1')
