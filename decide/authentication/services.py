c7abfe850b6d85cbefab6b1d2da26f96aca4ef7a
Pablo Pino <pabpinjim@alum.us.es>
2018-12-16 18:39:11 +0100

    El login mediante email en servicios usa el email y no un objeto User.
    Se ha introducido el método check_if_user_exists_email, que se encarga de
    obtener el usuario asociado al email que recibe como entrada y devuelve,
    o bien el usuario con el email, o bien un error de validación indicando
    que el usuario no existe.

--- decide/authentication/services.py
+++ decide/authentication/services.py
@@ -7,8 +7,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from Crypto import Random
 from rest_framework.authtoken.models import Token
 
-def send_mail_2_steps_auth(user):
-    u = check_if_user_exists(user)
+def send_mail_2_steps_auth(email):
+    u = check_if_user_exists_email(email)
     code = str(int.from_bytes(Random.new().read(24), 'big'))
     try:	
         old_two_steps_auth = TwoStepsAuth.objects.get(user = u)
@@ -18,8 +18,8 @@ def send_mail_2_steps_auth(user):
         two_steps_auth = TwoStepsAuth.objects.create(code = code, user = u)
     send_mail('Autenticación en Decide mediante email', ''.join(('Use el siguiente código para autenticarse en el sistema\r\n\r\n', str(code))), 'decide@decide.com', [user.email], fail_silently = False)
 
-def login_email_auth(user):
-    u = check_if_user_exists(user)
+def login_email_auth(email):
+    u = check_if_user_exists_email(email)
     try:
         token = Token.objects.get(user = u)
         raise ValidationError(_("El usuario ya está autenticado en el sistema"),
@@ -31,6 +31,14 @@ def login_email_auth(user):
         
 def check_if_user_exists(user):
     u = User.objects.get(pk = user.pk)
+    if u == None:
+        raise ValidationError(_("No se ha encontrado el usuario en la base de datos"),
+            code = '404_usuario',
+            params = {},)
+    return u
+
+def check_if_user_exists_email(email):
+    u = User.objects.get(email = email)
     if u == None:
         raise ValidationError(_("No se ha encontrado el usuario en la base de datos"),
             code = '404_usuario',
