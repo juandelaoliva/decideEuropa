583615b6cff8b214013f65b4e63c446f141e0875
Sergio Fernandez Jimenez <serferjim1598753@gmail.com>
2018-12-29 11:46:03 +0100

    Añadida condición de unicidad en el correo
    Se ha añadido en el modelo de usuario la condición de que el email sea único
    en la base de datos, añadiendo "unique=True" en el parametro del modelo.
    Este commit está relacionado con la Issue #37

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserDecide(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
-    email = models.EmailField()
+    email = models.EmailField(unique = True)
     user = models.OneToOneField(User, on_delete = models.CASCADE)
 
 class TwoStepsAuth(models.Model):
