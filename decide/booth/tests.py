c6ca276ef12151549089f2a74e2dfdc88740fd2b
Alejandro Santos <alesanmon1997@gmail.com>
2019-01-12 01:01:26 +0100

    INC CABINA #19 - Listado de votaciones
    Causa: Petición de decide-europa-autenticación para crear un listado de votaciones.
    Antes del cambio: No había ningún listado de las votaciones disponibles.
    Después del cambio: Ahora hay un listado de las votaciones con enlaces para acceder a sus cabinas de votación.

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import random
+from base.tests import BaseTestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+from base import mods
+
+# Create your testsfrom django.test import TestCase
+
+class TestBooth(BaseTestCase):
+    
+
+    def test_call_view_votings_authenticated(self):
+    
+        response = self.client.get('/booth/votings/', follow=True) 
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booth/votings.html')
+
+   
