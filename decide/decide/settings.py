07051923be840da53cce2e03662dd4bbdcc52a73
jhosue <jhosueyder@gmail.com>
2019-01-12 18:45:21 +0100

    Añadido modulos Se ha añadido modulo faltante

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
-    'django_forms_bootstrap'
     'bootstrap4',
+    'django_forms_bootstrap'
+
 
 ]
 
